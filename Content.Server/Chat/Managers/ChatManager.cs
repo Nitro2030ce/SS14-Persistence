30f8af3e09af19f76b7980b4f96681f6044031e5
Michael Chessall <chessall007@gmail.com>
2026-03-23 19:14:48 -0600

    Update ChatManager.cs

--- Content.Server/Chat/Managers/ChatManager.cs
+++ Content.Server/Chat/Managers/ChatManager.cs
@@ -116,7 +116,6 @@ internal sealed partial class ChatManager : IChatManager
     {
         var wrappedMessage = Loc.GetString("chat-manager-server-wrap-message", ("message", FormattedMessage.EscapeText(message)));
         ChatMessageToAll(ChatChannel.Server, message, wrappedMessage, EntityUid.Invalid, hideChat: false, recordReplay: true, colorOverride: colorOverride);
-        _sawmill.Info(message);
 
         _adminLogger.Add(LogType.Chat, LogImpact.Low, $"Server announcement: {message}");
     }
