16058925edf6129fdda9687813c7a43f6a19a28b
Nitro2030ce <nitro2030ce@gmail.com>
2026-04-26 17:00:33 -0400

    Delete runserver.sh

--- runserver.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-dotnet run --project Content.Server --config-file "/run/media/alexl/BigTuxMedia/Servers/SS14/SS14-Persistence/Resources/ConfigPresets/persistBase.toml"
-read -p "Press enter to continue"
